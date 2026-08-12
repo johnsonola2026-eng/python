f3c5c324c3559bf8609182b277c4b8c3cd67ab5c
emmanuel <jhonsonola2026@gmail.com>
2026-08-12 12:19:13 -0400

    type classing.py

---
+++ type classing.py
@@ -0,0 +1,12 @@
+name="Emmanuel"
+age=10
+is_student=True
+weight=70
+
+print("data type of name is", type(name))
+print("data type of age is", type(age))
+print("data type of is_student is", type(is_student))
+print("data type of weight is", type(weight))
+age=str(age)
+print(age)
+print(type(age))
