5980aa6ff6d15ed55af4c021616d811e3a9ed077
emmanuel <jhonsonola2026@gmail.com>
2026-08-14 11:30:08 -0400

    persentage.py

---
+++ persentage.py
@@ -0,0 +1,8 @@
+print("enter marks obtained in 4 subjects:")
+math=int(input("enter the value of math"))
+english=int(input("enterthe value of english"))
+writing=int(input("enter the value of writing"))
+sum=math+english+writing
+print("sum of marks are",sum)
+persentage=(sum/60)*100
+print(persentage)
