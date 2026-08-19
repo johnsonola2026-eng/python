66ce3daeea8539332c4142e700414a94ff0c9fb4
emmanuel <jhonsonola2026@gmail.com>
2026-08-19 11:10:55 -0400

    medical problems.py

---
+++ medical problems.py
@@ -0,0 +1,11 @@
+medical_cause=input("did you have a medical cause?(y/n)",)
+
+if ("y"==medical_cause):
+    print("yes, you're allowed")
+else:
+    atten=int(input("enter the atendent of this student:"))
+
+    if atten>=75:
+        print ("allowed")
+    else:
+        print("not allowed")
