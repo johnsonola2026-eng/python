5c7bac830ebf2e4501e2d472ba996a596b0fe54b
emmanuel <jhonsonola2026@gmail.com>
2026-08-20 11:14:45 -0400

    age classifacation.py

--- age classifacation.py
+++ age classifacation.py
@@ -1,6 +1,5 @@
 age = int(input("type age: "))
-if 1 <= age <= 9 or 21 <= age <= 100:
-    print("not allowed")
+if 10 <= age <=20:
+    print("success")
 else:
-    if 10 <= age <= 20:
-        print("allowed")
+        print("invaild")
