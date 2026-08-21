5a2a0005d48ab4b01ab980e4779ba1590fe6eb0d
emmanuel <jhonsonola2026@gmail.com>
2026-08-20 22:11:44 -0400

    power calculation.py

--- power calculation.py
+++ power calculation.py
@@ -1,7 +1,10 @@
-if n:=50:
-    string1=5
-    string2=10
-    string3=15
-    string4=20
+string1= 20
+string2= 10
+string3= 15
+string4= 20
+
+E = string1 + string2 + string3 + string4
+if E == 50:
     print("match")
-else:print("disable")
+else:
+    print("not match")
