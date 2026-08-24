9433d63885e9de018ee05992624d52f1603d2532
emmanuel <jhonsonola2026@gmail.com>
2026-08-24 11:21:02 -0400

    square root2.py

--- square root2.py
+++ square root2.py
@@ -1,7 +1,7 @@
-x = int(input("Enter a number from 1-10: "))
-y = int(input("Enter a number from 1-10: "))
+x = int(input("Enter a number from 1-100:"))
+y = int(input("Enter a number from 1-100   "))
 
-if (1 <= x <= 10) and (1 <= y <= 10):
+if (1 <= x <= 100) and (1 <= y <= 100):
     print(f"x={x}, y={y} x*y={x*y}")
 else:
     print("Invalid input")
